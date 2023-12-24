895ede819fec4b053d37d657993e62f637880107
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-12-24 17:24:47 +0530

    defined USER table

--- server/db_schema.py
+++ server/db_schema.py
@@ -8,3 +8,32 @@ import os
 
 
 load_dotenv()
+Base = declarative_base()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+
+############################################################
+######################### USER TABLE #######################
+############################################################
+
+
+class USER(Base):
+    __tablename__ = "USER"
+    id = Column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)
+    uid = Column(VARCHAR(36), nullable=False)
+    first_name = Column(VARCHAR(36), nullable=False)
+    last_name = Column(VARCHAR(36), nullable=False)
+    email = Column(VARCHAR(100), nullable=False)
+    username = Column(VARCHAR(36), nullable=False)
+    password = Column(VARCHAR(64), nullable=False)
+    created_at = Column(DATETIME, nullable=False)
+
+
+############################################################
+##################### CONVERSATION TABLE ###################
+############################################################
