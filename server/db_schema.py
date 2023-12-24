b1fe702e0e57700daedac2b2a306cba296d844ec
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-12-24 09:39:50 +0530

    intial push, default code cleanup

---
+++ server/db_schema.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR, DATETIME
+from dotenv import load_dotenv
+import os
