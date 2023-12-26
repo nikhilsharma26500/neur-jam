366e81b3fed73342669809c2a70810fb42e97bae
Nikhil Sharma <nikhilsharma26500@outlook.com>
2023-12-26 19:00:58 +0530

    added openai route and handler functions

--- server/openai_handler.py
+++ server/openai_handler.py
@@ -1,8 +1,24 @@
-from openai  import AsyncOpenAI
+from openai  import OpenAI
 
 class OpenAIHandler:
     
     def get_response_openAI(self, model: str, query: str, api_key: str):
-        client = AsyncOpenAI()
-        response = client.chat.completions
+        client = OpenAI(api_key=api_key)
+        response = client.chat.completions.create(
+            model=model,
+            response_format={"type": "text"},
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Good bot"
+                },
+                {
+                    "role": "user",
+                    "content": query
+                }
+            ]
+        )
+        
+        output = response.choices[0].message.content
+        return output
 
