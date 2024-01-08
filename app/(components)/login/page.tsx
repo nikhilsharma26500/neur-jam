996df4de8b7bfab8c2d1b1e1bf6509944a390c3f
Nikhil Sharma <nikhilsharma26500@outlook.com>
2024-01-08 22:29:37 +0530

    prepared folder structure for login and signup (frontend)

---
+++ app/(components)/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const login = () => {
+    return (
+        <>
+            <div>
+                <h1>Hi there!</h1>
+                <h2>Please login by filling your details below</h2>
+            </div>
+        </>
+    )
+}
+
+export default login
